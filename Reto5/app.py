c1551d4711827f66e46ee6502aa5742c8823afdf
Frey Usuga <fausuga673@misena.edu.co>
2022-08-05 14:10:42 -0500

    Los múltiplos de 3 seguían apareciendo. Corregido

--- Reto5/app.py
+++ Reto5/app.py
@@ -16,22 +16,21 @@ while i != num:         # mientras i sea diferente de num...
     i += 1              # ...i va incrementar y cuando sea igual a num, el ciclo se detendrá
     primos.append(i)    # i se almacena en 'primos'
 
-for par in primos:          # recorremos la información de 'primos'
-    if par % 2 == 0:        # dividimos par entre 2, si el resto es igual a 0...
-        primos.remove(par)  # ...removemos los números pares.
-
 while primos[p] * primos[p] < num:      # mientras lo potencia de la posición actual sea menor a num
-    p += 1 # la posición incrementa
+    p += 1                              # p incrementa
     for impares in primos:              # recorremos los datos de 'primos'
         if impares % primos[p] == 0:    # se divide impares por la actual posición de primos, si el resto es igual a 0...
             primos.remove(impares)      # ...dejamos solo los números primos   
         if num in primos:               
             primos.remove(num)          # en caso de que el número que ingremos se encuentre en 'primos' lo removemos   
 
+for par in primos:          # recorremos la información de 'primos'
+    if par % 2 == 0:        # dividimos par entre 2, si el resto es igual a 0...
+        primos.remove(par)  # ...removemos los números pares.
+
 primos = primos[1:] # eliminamos el número 1 ya que no es primo.
 
-# con el código que hemos usado anteriormente, eliminamos el 2 y el 3, como son números primos tenemos que volver a ingresarlos.
-primos.insert(0, 3)
+# 2 es considerado primo por lo que tendremos que volver a ingresarlo.
 primos.insert(0, 2)
 
 primosRev = primos[::-1]         # invertimos los datos de primos
